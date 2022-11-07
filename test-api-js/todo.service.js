71c9c7ae0206e288d3988860ace10d7dbea3fd3b
Varsha Reddy Banda <varshareddybanda@Varshas-Air.attlocal.net>
2022-11-06 20:37:37 -0600

    Added functions and testcases in code

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -23,17 +23,29 @@ class todoservice{
     }
 
     add_todo(todo){
-        // Your code here
+        this.todos.todo.push(todo);
+        return this.todos;
     }
 
     delete_todo(id){
-        // Your code here
+        this.todos.todo.splice(id,1);
+        return this.todos;
     }
 
     update_todo(id, todo){
-        // Your code here
+       this.todos.todo[id]["title"]=todo["title"];
+       this.todos.todo[id]["description"]=todo["description"];
+       this.todos.todo[id]["done"]=todo["done"];
+        return this.todos;
     }
+   
 }
+const a = new todoservice();
+/*a.add_todo({"id":4,"title":"dummy","done":true})*/
+/*a.delete_todo(2,{"id":2,"title":"T1","done":false})*/
+/*a.update_todo(2,{"id":2,"title":"sunny","done":false}) */
+
+console.log(a.get_todos())
 
 
 module.exports= todoservice;
--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -22,6 +22,21 @@ describe('todo test suite', () => {
     
 
     // Write all your test cases here that corresponds to software requirements
+    //Default lenght is 3 after adding a element to list it would be 4
+    test("add_todo", () => {
+        expect(todo_service.add_todo(todo1).todo.length).toEqual(4);
+    });
 
+
+ //  length of the todo list after adding is 4 //after deleting it would be 3.
+    test("delete_todo", () => {
+        expect(todo_service.delete_todo(1).todo.length).toEqual(3);
+    });
+
+
+    // Updating todo of id 1 with data from custom todo input and checking if it's equal to provided data
+    test("update_todo", () => {
+        expect(todo_service.delete_todo(1,todo1).todo[1]["title"]).toEqual("T4");
+    });
 
 });
